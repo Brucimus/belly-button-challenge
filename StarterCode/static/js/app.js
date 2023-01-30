3943827fdef7b50e382bbcef8a76987f8927cae8
Brucimus <brucimus@gmail.com>
2023-01-29 17:25:23 -0800

    clean up code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,21 +2,27 @@ function init() {
   getData();
 }
 
+
+// Get data for initial creation of the page
 function getData() {
   let bblint = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-  // console.log("This2")
 
     d3.json(bblint).then(dataSeparated)
 
 };
 
+// Get data for when the on change event occurs.  Unsure why the script wouldn't let me establish both getDatas into one, but it didn't seem to work, so I split it up.  Rework later.
 function getData2() {
   let lintlink = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
   d3.json(lintlink).then(refreshData)
 };
 
+
+// on change listener
 d3.selectAll("#selDataset").on("change", getData2);
 
+
+// populates data and dropdown menu on initial page load
 function dataSeparated(data){
 
     console.log(data)
@@ -32,6 +38,29 @@ function dataSeparated(data){
 
 };
 
+// repopuates data upon change selection
+function refreshData(data) {
+  let dropdownMenu = d3.select("#selDataset")
+  let dataset = dropdownMenu.property("value")
+  console.log(dataset)
+  let samples = Object.values(data.samples)
+  createGraphs(dataset,samples)
+
+  let metaData = Object.values(data.metadata);
+  cardData(dataset,metaData);
+};
+
+// populates drop down menu
+function createOptions(namesList){
+  let optionslist = ""
+  for (let i = 0; i<namesList.length; i++) {
+    optionslist = optionslist + `<option value="${namesList[i]}">${namesList[i]}</option>`
+  }
+  
+  d3.select("#selDataset").html(optionslist)
+};
+
+// changes card data
 function cardData(metaArg,objectArray) {
   let cardBody = ""
   let objectItem = objectArray.filter(x => x.id == metaArg);
@@ -44,26 +73,7 @@ function cardData(metaArg,objectArray) {
   d3.select(".panel-body").html(cardBody)
 };
 
-function refreshData(data) {
-  let dropdownMenu = d3.select("#selDataset")
-  let dataset = dropdownMenu.property("value")
-  console.log(dataset)
-  let samples = Object.values(data.samples)
-  createGraphs(dataset,samples)
-
-  let metaData = Object.values(data.metadata);
-  cardData(dataset,metaData);
-};
-
-function createOptions(namesList){
-    let optionslist = ""
-    for (let i = 0; i<namesList.length; i++) {
-      optionslist = optionslist + `<option value="${namesList[i]}">${namesList[i]}</option>`
-    }
-    
-    d3.select("#selDataset").html(optionslist)
-};
-
+// populates bar and bubble graphs
 function createGraphs(nameArg, objectArray) {
    
     let objectItem = objectArray.filter(x => x.id == nameArg);
@@ -103,8 +113,9 @@ function createGraphs(nameArg, objectArray) {
 
 }
 
-function optionChanged(value) {
-  // console.log(value);
+// define optionChanged inline function
+function optionChanged() {
 };
 
+// invoke init function
 init();
